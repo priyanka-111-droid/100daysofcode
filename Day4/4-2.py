88dd3967fe5f6f6842afb8c25f7107cbc425639a
priyanka-111-droid <77121296+priyanka-111-droid@users.noreply.github.com>
2021-05-22 23:35:24 +0530

    described lists

--- Day4/4-2.py
+++ Day4/4-2.py
@@ -1,11 +1,16 @@
 #LIST DATA STRUCTURE
-#fruits=["apple","mango"]
+#data structure is a way to organize and store data.Examples in python are lists,tuples,sets,dictionaries
+#Now let us see lists
+#fruits=["apple","mango","grapes"] 
 #list offsets/index can be positive or negative in python
+#example list fruits has 3 elements and index of leftmost element is 0 and rightmost is 2(number of elements - 1)
+#But we can also start from rightmost element of the list so index of rightmost element is -1 and then -2 and so on
 #print(fruits[0]) #Here 0 is the offset/index and apple is printed
-#fruits.append("Cherry") will attach "Cherry"to end of list
-#fruits.extend(["Papaya","Pineapple"]) to add another list to end of fruits list
-#fruits.remove("Cherry") 
-#fruits.pop([1]) pop and returns mango
+#print(fruits[-1]) #Here grapes is printed
+#fruits.append("Cherry") # append will attach "Cherry"to end of list 
+#fruits.extend(["Papaya","Pineapple"])  # extend adds another list to end of fruits list
+#fruits.remove("Cherry") #remove deletes the item Cherry but DOES NOT return deleted value so it cannot be stored in another variable
+#popped_value=fruits.pop([1]) #pop removes element at index 1(mango) and returns it so it can be stored in popped_value
 
 #########<<<<PROGRAM>>>>##########
 '''Banker Roulette-Who will pay the bill? Give names as input and pick random name'''
@@ -19,8 +24,6 @@ print(f"{names[n]} has to pay the bill today.")  #print name at that index in na
 #Better way:use random.choice() to pick an item from list or sequence
 print(f"{random.choice(names)} has to pay the bill today.")
 
-#Mistakes:
-#Try to make input user generated instead of predefined lists
-#don't use for loops unnecessarily here
-
+#Mistake:
+#Don't need to use for loop to go through list here.
 
