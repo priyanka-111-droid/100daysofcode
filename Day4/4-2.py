2cc9c29345396d63f963367338f5ed2d015021c1
priyanka-111-droid <77121296+priyanka-111-droid@users.noreply.github.com>
2021-05-22 17:30:00 +0530

    changes

--- Day4/4-2.py
+++ Day4/4-2.py
@@ -1,19 +1,20 @@
 #LIST DATA STRUCTURE
-#fruits=[item1,item2]
-#print(fruits[0]) list offsets can be positive or negative in python
-#fruits.append("Cherry")
-#fruits.extend(["Papaya","Pineapple"])
-#fruits.remove("Cherry")
-#fruits.pop([1]) pop and return it
+#fruits=["apple","mango"]
+#list offsets/index can be positive or negative in python
+#print(fruits[0]) #Here 0 is the offset/index and apple is printed
+#fruits.append("Cherry") will attach "Cherry"to end of list
+#fruits.extend(["Papaya","Pineapple"]) to add another list to end of fruits list
+#fruits.remove("Cherry") 
+#fruits.pop([1]) pop and returns mango
 
 #########<<<<PROGRAM>>>>##########
-'''Banker Roulette-Who will pay the bill? give names as input and pick random name
-'''
+'''Banker Roulette-Who will pay the bill? Give names as input and pick random name'''
+
 import random
 input_str=input("give me all names comma separated")
 names=input_str.split(',')  #is now a list
-n=random.randint(0,(len(names)-1))
-print(f"{names[n]} has to pay the bill today.")
+n=random.randint(0,(len(names)-1)) #pick random integer
+print(f"{names[n]} has to pay the bill today.")  #print name at that index in names list
 
 #Better way:use random.choice() to pick an item from list or sequence
 print(f"{random.choice(names)} has to pay the bill today.")
