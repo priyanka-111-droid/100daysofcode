e869d03d3e20ca80c7ee36f52196e69cd33ad9ea
priyanka-111-droid <77121296+priyanka-111-droid@users.noreply.github.com>
2021-05-22 22:47:34 +0530

    changing

--- Day1/1-4.py
+++ Day1/1-4.py
@@ -4,10 +4,11 @@ b=input("b:")
 #METHOD 1:direct swap using 2 variables(more pythonic)
 #a,b=b,a
 
-#METHOD 2:swapping using third temporary variable
-c=a
-a=b
-b=c 
+#METHOD 2:swapping using third temporary variable,in this case,c
+#Remember,assignment operator(=) assigns values on the right to the variable on its left
+c=a  #value of a is stored in c
+a=b  #value of b is stored in a
+b=c  #value in c is now stored in b
 #print swapped values
 print("a:"+a)
 print("b:"+b)
