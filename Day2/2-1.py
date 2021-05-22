78a33fc7c1f4a05e120203e3d3fe4242c3e89431
priyanka-111-droid <77121296+priyanka-111-droid@users.noreply.github.com>
2021-05-22 22:35:25 +0530

    explanation

--- Day2/2-1.py
+++ Day2/2-1.py
@@ -1,12 +1,13 @@
-#DATATYPES
-#STRING #subscript
-print("Hello"[0]) 
+#DATATYPES - are classification of data items
+#can be string(str),integer(int),floating point number(float),boolean(bool)
+#STRING subscript
+print("Hello"[0]) # H
 #concat
-print("123"+"345")
+print("123"+"345")# 123345
 #INTEGER
 print(123+456)
-#len(4867) gives ERROR
-#FLOAT
+#len(4867) gives ERROR as 4867 is an integer not string
+#FLOAT-decimal values
 #BOOLEAN-True , False
 
 #HOW TO FIND OUT WHAT TYPE?
@@ -19,11 +20,13 @@ print(type(new_num_char))
 
 
 ##################<<PROGRAM>>###################
-'''out of random 2 digit number,add 2 digits'''
+'''Input a 2 digit number,add its 2 digits'''
 
 num=int(input("Enter two digit number :"))
 print(int(str(num)[0])+int(str(num)[1]))
 
+#We use str() type conversion to extract digit at 0th subscript and 1st subscript 
+#Then we convert it to int because we need to add the two digits .
 #concat string and int gives error!!!
 
 
