01cf50e5a082875d8d6e708ab669e6a0929ab376
priyanka-111-droid <77121296+priyanka-111-droid@users.noreply.github.com>
2021-05-22 17:21:21 +0530

    commenting styles

--- Day1/1-3.py
+++ Day1/1-3.py
@@ -3,3 +3,8 @@
 #input nesting inside print
 #use len() to get number of char or length of string
 print(len(input("Please enter your name:")))
+
+#single line comments are used with # symbol
+#multi-line comments can be done using triple quoted multiline strings (''' ''') used to usually describe what  program does in the beginning but be sure to indent it properly
+'''This is a multi-line comment'''
+"""This is also a multi-line comment"""
