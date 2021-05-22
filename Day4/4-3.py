206e9e27082aa0860e1c0bf4792d1092f03452a7
priyanka-111-droid <77121296+priyanka-111-droid@users.noreply.github.com>
2021-05-22 23:22:19 +0530

    updates

--- Day4/4-3.py
+++ Day4/4-3.py
@@ -1,3 +1,7 @@
+#IndexOutOfRange error
+#Lets say we have a list l with number of elements n
+#index of left most element is 0 and index of last element is n-1 
+#If you try to print element at index [n] you get the error as last element in the list was present at index n-1.
 #NESTED LISTS
 # dirty_dozen=["Strawberries","Spinach","Kale","Nectarines","Apples","Grapes","Peaches","Cherries","Pears","Tomatoes","Celery","Potatoes"]
 # fruits=["Strawberries","Nectarines","Apples","Grapes","Peaches","Cherries","Pears"]
